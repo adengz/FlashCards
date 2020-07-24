7e8cb539a3f56bd8bb64c29786eba7fc4e265006
adengz <adengz@gmail.com>
2020-07-24 15:07:23 -0700

    feat: Implement new deck ui

--- src/components/NewDeck.js
+++ src/components/NewDeck.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Platform, StyleSheet } from 'react-native';
+import { useTheme, TextInput, Button } from 'react-native-paper';
+import { Styles } from '../styles/stylesheet';
 
 export default function NewDeck() {
+  const [title, setTitle] = useState('');
+  const { dark } = useTheme();
+
   return (
-    <View>
-      <Text>New Deck</Text>
+    <View style={[Styles.container, { justifyContent: 'center' }]}>
+      <TextInput
+        style={styles.textInput}
+        mode="outlined"
+        label="Title of new deck"
+        placeholder="Type something"
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Button
+        style={styles.btn}
+        labelStyle={styles.btnLabel}
+        mode="contained"
+        dark={!dark}
+        disabled={title === ''}
+        children="Submit"
+        uppercase={Platform.OS === 'android'}
+        onPress={() => console.log('submitted')}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 80,
+    fontSize: 32,
+    margin: 20,
+  },
+  btn: {
+    width: '50%',
+    alignSelf: 'center',
+    margin: 20,
+  },
+  btnLabel: {
+    fontSize: 24,
+  },
+});
--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,8 +1,12 @@
+import { Platform } from 'react-native';
 import { DefaultTheme, DarkTheme } from 'react-native-paper';
 import { black } from './palette';
 
+const roundness = Platform.OS === 'ios' ? 8 : 4;
+
 export const lightTheme = {
   ...DefaultTheme,
+  roundness,
   colors: {
     ...DefaultTheme.colors,
     primary: '#006A96',
@@ -13,6 +17,7 @@ export const lightTheme = {
 
 export const darkTheme = {
   ...DarkTheme,
+  roundness,
   colors: {
     ...DarkTheme.colors,
     primary: '#FFCD00',
