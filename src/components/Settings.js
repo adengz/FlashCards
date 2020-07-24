4640a2cb00c7356b4fa8ccd02699eeb6d70ae56d
adengz <adengz@gmail.com>
2020-07-23 23:12:19 -0700

    feat: Implement settings ui

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { StyleSheet, ScrollView, Switch } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text } from 'react-native-paper';
+import { useTheme, List, Divider } from 'react-native-paper';
 import { Styles } from '../styles/stylesheet';
+import { red } from '../styles/palette';
 
 export default function Settings() {
+  const { surface } = useTheme().colors;
+
+  const styles = StyleSheet.create({
+    item: {
+      backgroundColor: surface,
+    },
+    titleDanger: {
+      color: red,
+      fontWeight: 'bold',
+    },
+  });
+
   return (
     <SafeAreaView style={Styles.container}>
-      <Text>Settings</Text>
+      <ScrollView>
+        <List.Section>
+          <List.Subheader>Appearances</List.Subheader>
+          <List.Item style={styles.item} title="Dark Mode" right={() => <Switch value={false} />} />
+        </List.Section>
+        <Divider />
+        <List.Section>
+          <List.Subheader>DANGER ZONE</List.Subheader>
+          <List.Item
+            title="CLEAR DATA"
+            style={styles.item}
+            titleStyle={styles.titleDanger}
+            onPress={() => console.log('CLEAR DATA')}
+          />
+        </List.Section>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- src/styles/stylesheet.js
+++ src/styles/stylesheet.js
@@ -3,8 +3,5 @@ import { StyleSheet } from 'react-native';
 export const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 5,
-    justifyContent: 'space-between',
-    alignItems: 'center',
   },
 });
