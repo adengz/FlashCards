cafb00fa7f6b1a0b859a90a85f112fe6e6b16bd9
adengz <adengz@gmail.com>
2020-07-26 19:04:47 -0700

    feat: Set up redux for handling deck list sorting

--- src/redux/actions/settings.js
+++ src/redux/actions/settings.js
@@ -1,5 +1,16 @@
 export const TOGGLE_DARK = 'TOGGLE_DARK';
+export const REVERSE_DECK_ORDER = 'REVERSE_DECK_ORDER';
+export const UPDATE_DECK_SORTING = 'UPDATE_DECK_SORTING';
 
 export const toggleDark = () => ({
   type: TOGGLE_DARK,
+});
+
+export const reverseDeckOrder = () => ({
+  type: REVERSE_DECK_ORDER,
+});
+
+export const updateDeckSorting = (by) => ({
+  type: UPDATE_DECK_SORTING,
+  by,
 });
--- src/redux/reducers/settings.js
+++ src/redux/reducers/settings.js
@@ -1,6 +1,12 @@
-import { TOGGLE_DARK } from '../actions/settings';
+import { TOGGLE_DARK, REVERSE_DECK_ORDER, UPDATE_DECK_SORTING } from '../actions/settings';
 
-const defaultState = { dark: false };
+const defaultState = {
+  dark: false,
+  sortDecks: {
+    by: 'timestamp',
+    descending: false,
+  },
+};
 
 export default function settings(state = defaultState, action) {
   switch (action.type) {
@@ -8,6 +14,22 @@ export default function settings(state = defaultState, action) {
       return {
         ...state,
         dark: !state.dark,
+      };
+    case REVERSE_DECK_ORDER:
+      return {
+        ...state,
+        sortDecks: {
+          ...state.sortDecks,
+          descending: !state.sortDecks.descending,
+        },
+      };
+    case UPDATE_DECK_SORTING:
+      return {
+        ...state,
+        sortDecks: {
+          ...state.sortDecks,
+          by: action.by,
+        },
       };
     default:
       return state;
