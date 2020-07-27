8cffa90de95940afea6928e32392e3af008ccd69
adengz <adengz@gmail.com>
2020-07-27 12:47:42 -0700

    feat: Implement portal to float the modal above all elements

--- src/components/DeckSortingOptions.js
+++ src/components/DeckSortingOptions.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useTheme, Modal, RadioButton } from 'react-native-paper';
+import { useTheme, Portal, Modal, RadioButton } from 'react-native-paper';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateDeckSorting } from '../redux/actions/settings';
 
 export default function DeckSortingOptions({ visible, hide }) {
   const {
     roundness,
-    colors: { background, primary },
+    colors: { surface, primary },
   } = useTheme();
   const { by } = useSelector(({ settings }) => settings.sortDecks);
   const dispatch = useDispatch();
@@ -18,16 +18,18 @@ export default function DeckSortingOptions({ visible, hide }) {
   };
 
   return (
-    <Modal
-      contentContainerStyle={{ backgroundColor: background, margin: 10, borderRadius: roundness }}
-      visible={visible}
-      onDismiss={hide}
-    >
-      <RadioButton.Group value={by} onValueChange={(value) => select(value)}>
-        <RadioButton.Item label="Creation Time" color={primary} value="timestamp" />
-        <RadioButton.Item label="Title Alphabet" color={primary} value="title" />
-        <RadioButton.Item label="Total Cards" color={primary} value="cards" />
-      </RadioButton.Group>
-    </Modal>
+    <Portal>
+      <Modal
+        contentContainerStyle={{ backgroundColor: surface, margin: 10, borderRadius: roundness }}
+        visible={visible}
+        onDismiss={hide}
+      >
+        <RadioButton.Group value={by} onValueChange={(value) => select(value)}>
+          <RadioButton.Item label="Creation Time" color={primary} value="timestamp" />
+          <RadioButton.Item label="Title Alphabet" color={primary} value="title" />
+          <RadioButton.Item label="Total Cards" color={primary} value="cards" />
+        </RadioButton.Group>
+      </Modal>
+    </Portal>
   );
 }
