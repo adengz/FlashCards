15599a04a7a15c8ba0c17dc307cbb7ae2095b69b
adengz <adengz@gmail.com>
2020-08-04 16:36:28 -0700

    refactor: Use async function

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -2,46 +2,44 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 const SETTINGS_STORAGE_KEY = '@FlashCards:settings';
 
-export const initiateSettingsAsync = (defaultSettings) => {
+export const initiateSettingsAsync = async (defaultSettings) => {
   return AsyncStorage.setItem(SETTINGS_STORAGE_KEY, JSON.stringify(defaultSettings));
 };
 
-export const fetchSettingsAsync = () => {
-  return AsyncStorage.getItem(SETTINGS_STORAGE_KEY).then((str) => JSON.parse(str));
+export const fetchSettingsAsync = async () => {
+  const str = await AsyncStorage.getItem(SETTINGS_STORAGE_KEY);
+  return JSON.parse(str);
 };
 
-const updateSettingsAsync = (settings) => {
+const updateSettingsAsync = async (settings) => {
   return AsyncStorage.mergeItem(SETTINGS_STORAGE_KEY, JSON.stringify(settings));
 };
 
-export const toggleDarkAsync = () => {
-  return fetchSettingsAsync().then((settings) =>
-    updateSettingsAsync({ ...settings, dark: !settings.dark })
-  );
+export const toggleDarkAsync = async () => {
+  const settings = await fetchSettingsAsync();
+  return updateSettingsAsync({ ...settings, dark: !settings.dark });
 };
 
-export const reverseDeckOrderAsync = () => {
-  return fetchSettingsAsync().then((settings) => {
-    const newSettings = {
-      ...settings,
-      sortDecks: {
-        ...settings.sortDecks,
-        descending: !settings.sortDecks.descending,
-      },
-    };
-    return updateSettingsAsync(newSettings);
-  });
+export const reverseDeckOrderAsync = async () => {
+  const settings = await fetchSettingsAsync();
+  const newSettings = {
+    ...settings,
+    sortDecks: {
+      ...settings.sortDecks,
+      descending: !settings.sortDecks.descending,
+    },
+  };
+  return updateSettingsAsync(newSettings);
 };
 
-export const updateDeckSortingAsync = (by) => {
-  return fetchSettingsAsync().then((settings) => {
-    const newSettings = {
-      ...settings,
-      sortDecks: {
-        ...settings.sortDecks,
-        by,
-      },
-    };
-    return updateSettingsAsync(newSettings);
-  });
+export const updateDeckSortingAsync = async (by) => {
+  const settings = await fetchSettingsAsync();
+  const newSettings = {
+    ...settings,
+    sortDecks: {
+      ...settings.sortDecks,
+      by,
+    },
+  };
+  return updateSettingsAsync(newSettings);
 };
